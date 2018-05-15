d7478cc4ab5f27ecb68948d677d23709c2954ef7
Brett <tianyi.li2@gmail.com>
2018-05-14 22:27:41 -0700

    test pymongo, but failed.

--- scrapBookWebSite.py
+++ scrapBookWebSite.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 # get the latest book id
 allCategoryUrl = "http://www.yousuu.com/category/all"
@@ -9,9 +10,21 @@ soup = BeautifulSoup(pageData, 'lxml')
 
 latest = soup.select_one("div[class=post]")
 address = latest.contents[0].__str__()
-pattern = re.compile(r'\d+')
-bookId = pattern.findall(address)
-print(bookId[0])
+pattern = re.compile('\d+')
+lastBook = pattern.findall(address)
+
+
+# get all id with official website and store in MongoDB
+baseUrl = "http://www.yousuu.com/book/"
+
+client = MongoClient('localhost', 27017)
+db = client.books
+
+testBook = {"bookId" : 0,
+             "bookName" : "测试"}
+
+db.books.insert_one(testBook)
+
 
 
 
