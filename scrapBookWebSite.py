b098697f3e23c3e98a72753902fa4b12ccd67d19
Brett <tianyi.li2@gmail.com>
2018-05-14 18:29:09 -0700

    Get latest book ID.

--- scrapBookWebSite.py
+++ scrapBookWebSite.py
@@ -1,1 +1,17 @@
-print("test")
+import requests
+import re
+from bs4 import BeautifulSoup
+
+# get the latest book id
+allCategoryUrl = "http://www.yousuu.com/category/all"
+pageData = requests.get(allCategoryUrl).text
+soup = BeautifulSoup(pageData, 'lxml')
+
+latest = soup.select_one("div[class=post]")
+address = latest.contents[0].__str__()
+pattern = re.compile(r'\d+')
+bookId = pattern.findall(address)
+print(bookId[0])
+
+
+
